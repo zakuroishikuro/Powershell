a25a24451260697efdcad37eb94dc978bb5729df
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2023-12-24 00:13:11 +0900

    記述をシンプルにする

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,5 @@
 # ./modules/ 配下のモジュールを読み込む
-Get-ChildItem "$PSScriptRoot\modules" -Filter *.psm1 | % {
-  Import-Module $_.FullName
-}
+Get-ChildItem "$PSScriptRoot\modules" -Filter *.psm1 | ForEach-Object { Import-Module $_.FullName }
 
 Set-Alias open explorer.exe
 Set-Alias which where.exe
@@ -12,7 +10,5 @@ Set-Alias xml ConvertTo-Xml
 Set-Alias html ConvertTo-Html
 
 function prompt() {
-  # ホームディレクトリは「~」に置換
-  $d = (Get-Location).Path.Replace($HOME, "~")
-  "$(clockEmojiNow) $($d)> "
+  "$(clockEmojiNow) $((Get-Location).Path.Replace($HOME, "~"))> "
 }
