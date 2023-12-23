2f8fea4280922875bf362ef862356aa88fec1977
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2023-12-24 06:57:39 +0900

    windowsの初期設定用のモジュールを追加

---
+++ modules/initialize_windows.ps1
@@ -0,0 +1,18 @@
+function initialize_windows() {
+  winget install -eh Microsoft.PowerShell
+  winget install -eh Microsoft.VisualStudioCode
+  winget install -eh Git.Git
+  
+  winget install -eh Logseq.Logseq
+  
+  wignet install -eh OpenJS.NodeJS
+  winget install -eh RubyInstallerTeam.Ruby.3.2
+  winget install -eh Python.Python.3.12
+  
+  #winget install -eh Google.Chrome
+  #winget install -eh Mozilla.Firefox
+  #winget install -eh Obsidian.Obsidian
+  #winget install -eh VideoLAN.VLC
+  #winget install -eh Google.AndroidStudio
+}
+
