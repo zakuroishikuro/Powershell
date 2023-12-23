ca3e9d30063ed73fddc51f29318a81078be4a5ae
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2023-12-24 00:12:01 +0900

    便利なエイリアスを追加

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,8 +3,13 @@ Get-ChildItem "$PSScriptRoot\modules" -Filter *.psm1 | % {
   Import-Module $_.FullName
 }
 
-# PowerShellだとwhereに.exeつけないと動かない。linuxと同じくwhichで動くようにしておく
+Set-Alias open explorer.exe
 Set-Alias which where.exe
+
+Set-Alias json ConvertTo-Json
+Set-Alias csv ConvertTo-Csv
+Set-Alias xml ConvertTo-Xml
+Set-Alias html ConvertTo-Html
 
 function prompt() {
   # ホームディレクトリは「~」に置換
