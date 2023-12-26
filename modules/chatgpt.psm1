49dc0ced18453aeec73e17cb4196275ca89350e1
zakuroishikuro <zakuroishikuro@users.noreply.github.com>
2023-12-27 06:39:28 +0900

    chatgptにヒストリ機能とrepl機能を追加
    セッション内なら会話を憶えてくれる

--- modules/chatgpt.psm1
+++ modules/chatgpt.psm1
@@ -1,11 +1,6 @@
-$global:ChatGptMessages = @(
-  @{
-    role    = "system"
-    content = "You are a helpful assistant."
-  }
-)
+$SYSTEM_CONTENT = "You are a helpful assistant."
 
-function chat($message) {
+function chatgpt($message) {
   $apiKey = $env:CHAT_GPT_API_KEY
   if ($null -eq $apiKey) {
     Write-Output "CHAT_GPT_API_KEY is not set."
@@ -20,24 +15,59 @@ function chat($message) {
   }
 
   # 返答をpushする
-  $global:ChatGptMessages += @{
+  $script:ChatGptMessages += @{
     role    = "user"
     content = $message
   }
 
   $body = @{
     model    = "gpt-3.5-turbo"
-    messages = $global:ChatGptMessages
+    messages = $script:ChatGptMessages
   } | ConvertTo-Json
-
-  
 
   $response = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body $body
 
   $responsedMessage = $response.choices[0].message.content
-  $global:ChatGptMessages += @{
+  $script:ChatGptMessages += @{
     role    = "system"
     content = $responsedMessage
   }
   Write-Output $responsedMessage ""
 }
+
+function chat($messageArg) {
+  # messageがあればそれをchatgptに渡す
+  if ($messageArg) {
+    chatgpt $messageArg
+    return
+  }
+
+  #messageが空ならwhile-loopで何度もchatgptに質問可能にする
+  while ($true) {
+    $message = Read-Host " * chat> "
+    # 文字列が空なら終了
+    if ($message) {
+      chatgpt $message
+    }
+    else {
+      break
+    }
+  }
+}
+
+function chat_init {
+  $script:ChatGptMessages = @(
+    @{
+      role    = "system"
+      content = $SYSTEM_CONTENT
+    }
+  )
+}
+
+function chat_history {
+  $script:ChatGptMessages | ForEach-Object {
+    Write-Output " * $($_.role): $($_.content)"
+  }
+}
+
+chat_init
